71bcec1d9119e28b76e7f6be9fd5034084d33c03
MaryJud <81223450+MaryJud@users.noreply.github.com>
2021-12-26 16:29:52 +0100

    Modifica
    Modifica Profilo

--- src/views/profilo.js
+++ src/views/profilo.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React,{Component} from 'react';
 import "../App.css";
-import {Form} from 'react-bootstrap'
+import {Card, Form} from 'react-bootstrap';
+import axios from 'axios';
+import './icss/profilo.css';
 
 
-export function Profilo(){
-    
-  return (
-      <div className="profilo">
-          <Form className="profilo" action="http://localhost:3000/api/admin/findAll" method="GET">
-          <h1>AREA PERSONALE</h1>
-           
-            <div><label>Nome: </label> </div>
+export default class Profilo extends Component{
+        constructor(props){
+            super(props);
+
+            this.state={
+                utente:null,
+            }
+        }
+        
+        componentDidMount() {
+            axios.get('http://localhost:8080/api/profilo/findByEmail')
+              .then(response => {
+                this.setState({
+                  utente:response.data
+                })   
+              })
+              .catch(function (error) {
+                console.log(error);
+              })
+        
+      }
+     
+     render(){
+     return (
+      <Card className=" mx-auto col-xl-7 justify-content-center text-center">
+            <div className="row d-flex justify-content-center">
+            <div className="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+        
+            
+            <label>Nome: </label> 
+            <h1>{this.state.utente.nome}</h1>
             <div><label>Cognome: </label></div>
             <div><label>Indirizzo: </label></div>
             <div><label>Email: </label></div>
             
-            <Form className="profilo" action="http://localhost:3000/api/admin/findAll" method="POST">
+            <Form className="profilo" action="http://localhost:3000/api/profilo/updatePassword" method="POST">
                 <div><label>Modifica Password: </label></div>
                 <div>
-                    <input type="Mpassword" name="Mpassword" id="Mpassword"/>
-                    <input type="button" value="modifica" id="submit"/>
+                    <input type="bottone" name="Password"/>
                 </div>
             </Form>
-            </Form>
-            
-      </div>
+      
+            </div>    
+            </div>
+             
+      </Card>
   )
   }
-  export default Profilo
+}
 
