dd597a724085ff428644822a9539d9b26d4587b8
MaryJud <81223450+MaryJud@users.noreply.github.com>
2021-12-26 12:56:21 +0100

    Modifica profilo
    Modifica Profilo

--- src/views/profilo.js
+++ src/views/profilo.js
@@ -1,27 +1,31 @@
-import React,{Component} from "react";
-import {Form,Button,Col,Row} from "react-bootstrap";
+import React from 'react';
+import "../App.css";
+import {Form} from 'react-bootstrap'
 
-import '../App.css';
 
-export default class Profilo extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            nome:props.nome,
-        }
-    }
-    render(){
-    return(
-        <div className="container">
-        <Col className="profilo" action="http://localhost:3000/cliente/profilo" method="GET">
-            <Row>Nome</Row>{this.state.nome}
-            <Row>Email</Row>
-            <Button className="submitButton" variant="primary" type="submit">
-                ModificaPassword
-            </Button>
-        </Col>
-        </div>
-    )
-    }
-}
+export function Profilo(){
+    
+  return (
+      <div className="profilo">
+          <Form className="profilo" action="http://localhost:3000/api/admin/findAll" method="GET">
+          <h1>AREA PERSONALE</h1>
+           
+            <div><label>Nome: </label> </div>
+            <div><label>Cognome: </label></div>
+            <div><label>Indirizzo: </label></div>
+            <div><label>Email: </label></div>
+            
+            <Form className="profilo" action="http://localhost:3000/api/admin/findAll" method="POST">
+                <div><label>Modifica Password: </label></div>
+                <div>
+                    <input type="Mpassword" name="Mpassword" id="Mpassword"/>
+                    <input type="button" value="modifica" id="submit"/>
+                </div>
+            </Form>
+            </Form>
+            
+      </div>
+  )
+  }
+  export default Profilo
 
