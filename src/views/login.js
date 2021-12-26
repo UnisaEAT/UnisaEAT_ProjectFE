7c49e8181cc617e2bdae33ee41d9b97f9333a72d
MaryJud <81223450+MaryJud@users.noreply.github.com>
2021-12-26 17:41:05 +0100

    Modifica Login
    Login

--- src/views/login.js
+++ src/views/login.js
@@ -1,32 +1,41 @@
 import React from "react";
-import {Form,Button,Col,Row} from "react-bootstrap";
+import {Form,Button,Col,Row,Card} from "react-bootstrap";
 import '../App.css';
+import './icss/login.css';
 
 export function Login()
 {
     return(
-        <div className="formContainer container">
-            <Form className="test" action="http://localhost:3000/Login/login" method="POST">
-                <h3>LOGIN</h3>
-                <Row className="mb-3">
+        
+        <Card className=" mx-auto col-xl-7 justify-content-center text-center">
+        <div className="row d-flex justify-content-center">
+        <div className="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+        <div className="login">
+        
+            <Form className="test" action="http://localhost:3000/api/login/login" method="POST">
+                <h3>LOGIN</h3><br></br>
+                <Row>
                     <Form.Group as={Col}>
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="text" name="email" placeholder="Inserisci la tua email" />
                     </Form.Group>
                 </Row>
 
-                <Row className="mb-3">
+                <Row>
                     <Form.Group as={Col}>
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" name="password" placeholder="Inserisci la tua password" />
                     </Form.Group>
                 </Row>
                 <br/>
-                <Button className="submitButton" variant="primary" type="submit">
-                    Login
+                <Button className="bottone" variant="primary" type="submit">
+                    LOGIN
                 </Button>
             </Form>
         </div>
+        </div>
+        </div>
+        </Card>
     )
 }
 
