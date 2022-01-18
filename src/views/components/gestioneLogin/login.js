7a1a658d3f22f5e14ef85fc129ad0d97a27a1ee7
MaryJud <81223450+MaryJud@users.noreply.github.com>
2022-01-18 12:30:49 +0100

    Grafica login

--- src/views/components/gestioneLogin/login.js
+++ src/views/components/gestioneLogin/login.js
@@ -1,11 +1,13 @@
 import React from "react";
-import {Form, Button, Card} from "react-bootstrap";
+import {Form, Button, Card,Image} from "react-bootstrap";
 import '../../../App.css';
 import '../../styles/gestioneAutenticazione/login.css';
 import axios from 'axios';
 import {Component} from "react";
 import Popup from "../App/successPopUp";
 import {Redirect} from 'react-router-dom';
+import logoMail from "../../assets/email.png"
+import logoPsw from "../../assets/key.png"
 
 export default class Login extends Component {
 
@@ -120,65 +122,30 @@ export default class Login extends Component {
                      {/* Se popUp (boolean) è true */}
                      {this.state.popUp && <Popup message={'Login effettuato!'} handleClose={this.closePopUp}/>}
 
-                    {/*<div class="wrapper">
-                        <form action="#">
-                            <div class="h5 font-weight-bold text-center mb-3">Registration</div>
-                            <div class="form-group d-flex align-items-center">
-                                <div class="icon"><span class="far fa-user"></span></div> <input autocomplete="off" type="text" class="form-control" placeholder="Name"></input>
-                            </div>
-                            <div class="form-group d-flex align-items-center">
-                                <div class="icon"><span class="far fa-envelope"></span></div> <input autocomplete="off" type="email" class="form-control" placeholder="Email"></input>
-                            </div>
-                            <div class="form-group d-flex align-items-center">
-                                <div class="icon"><span class="fas fa-phone"></span></div> <input autocomplete="off" type="tel" class="form-control" placeholder="Phone"></input>
-                            </div>
-                            <div class="form-group d-flex align-items-center">
-                                <div class="icon"><span class="fas fa-map-marker-alt"></span></div> <input autocomplete="off" type="text" class="form-control" placeholder="City"></input>
-                            </div>
-                            <div class="form-group d-flex align-items-center">
-                                <div class="icon"><span class="fas fa-key"></span></div> <input autocomplete="off" type="password" class="form-control" placeholder="Password"></input>
-                                <div class="icon btn"><span class="fas fa-eye-slash"></span></div>
-                            </div>
-                            <div class="mb-2"> <label class="option">Remember me <input type="checkbox" checked></input> <span class="checkmark"></span> </label> </div>
-                            <div class="btn btn-primary mb-3">Signup</div>
-                            <div class="terms mb-2"> By clicking "Signup", you acknowledge that you have read the <a href="#">Privacy Policy</a> and agree to the <a href="#">Terms of Service</a>. </div>
-                            <div class="connect border-bottom mt-4 mb-4"></div>
-                            <ul class="p-0 social-links">
-                                <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
-                                <li><a href="#"><span class="fab fa-google"></span></a></li>
-                                <li><a href="#"><span class="fab fa-github"></span></a></li>
-                            </ul>
-                        </form>
-                    </div>*/}
-                    <Card className=" mx-auto col-xl-7 justify-content-center text-center position-inherit">
-                        <div className="row d-flex justify-content-center">
-                            <div className="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
-                                <div className="login">
-
+                   <Card className="wrapper">
+                                <div className="login h5 font-weight-bold text-center mb-3">
                                     <h1>LOGIN</h1>
-                                    <Form className="test" onSubmit={this.handleSubmit}>
+                                    <Form className="test align-items-center" onSubmit={this.handleSubmit}>
                                         <br/>
-                                        <div id="email">
-                                            <Form.Label className="label">Email</Form.Label>
-                                            <Form.Control id="email" className="control" type="text" name="email"
+                                        <div className="form-group align-items-center" id="email">
+                                        <div class="icon"> <Image src={logoMail} width="30"/></div>
+                                            <Form.Control id="email" className="form-control" type="text" name="email"
                                                           onChange={this.onChangeEmail}
                                                           placeholder="Inserisci la tua email"/>
                                         </div>
-                                        <div id="password">
-                                            <Form.Label className="label">Password</Form.Label>
-                                            <Form.Control id="password" className="control" type="password"
+                                        <div className="form-group align-items-center" id="password">
+                                        <div class="icon"> <Image src={logoPsw} width="30"/></div>
+                                            <Form.Control id="password" className="form-control" type="password"
                                                           name="password" onChange={this.onChangePassword}
                                                           placeholder="Inserisci la tua password"/>
                                         </div>
-                                        <Button className="bottoneLogin position-inherit" variant="primary" type="submit" >
+                                        <Button className="bottoneLogin align-items-center"  type="submit" >
                                             LOGIN
                                         </Button>
                                     </Form>
 
 
                                 </div>
-                            </div>
-                        </div>
                         </Card>
                 </div>
             )
