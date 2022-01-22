b1201d9ba73bc42d106ee10ed8c9af09701a36ce
Alessandro-Cavaliere <alexcavaliere00@gmail.com>
2022-01-22 19:46:22 +0100

    Update InserimentoPersonale.js

--- src/views/components/gestionePersonale/InserimentoPersonale.js
+++ src/views/components/gestionePersonale/InserimentoPersonale.js
@@ -3,6 +3,7 @@ import "../../styles/gestionePersonale/InserimentoPersonale.css"
 import {Card} from "react-bootstrap";
 import axios from "axios";
 import SuccessPopUp from "../App/successPopUp";
+import FailurePopUp from "../App/failurePopUp";
 
 export default class InserimentoPersonale extends React.Component {
     constructor(props) {
@@ -17,6 +18,7 @@ export default class InserimentoPersonale extends React.Component {
             email: '',
             password: '',
             confermapassword: '',
+            failurePopUp:false,
             popUp: false,
             error:false
         }
@@ -33,11 +35,16 @@ export default class InserimentoPersonale extends React.Component {
         this.onChangeConfermaPassword = this.onChangeConfermaPassword.bind(this)
         this.errorHandler = this.errorHandler.bind(this)
         this.closePopUp = this.closePopUp.bind(this)
+        this.closeFailurePopUp= this.closeFailurePopUp.bind(this)
     }
 
+    closeFailurePopUp(){
+        this.setState({failurePopUp: false})
+        window.location.reload()
+    }
     closePopUp() {
         this.setState({popUp: false})
-        window.location.reload(false);
+        window.location.reload();
     }
 
     errorHandler(error) {
@@ -152,13 +159,17 @@ export default class InserimentoPersonale extends React.Component {
             axios.post('http://localhost:8080/api/personale/insert', personale)
                 .then(response => {
                     //Se l'inserimento è andato a buon fine
+                    if (response.data.message === false) {
+                        this.setState({failurePopUp: true})
+                    }
                     if (response.data.message === true) {
-                        console.log("1")
                         this.setState({popUp: true})
                     } else {
-                        console.log("3")
+                        console.log("111")
                         this.errorHandler(response.data)
                     }
+
+
                 })
                 .catch((error) => {
                     console.log(error);
@@ -170,9 +181,11 @@ export default class InserimentoPersonale extends React.Component {
             return <h1 className="erroreGenericoDiAccesso">Effettua il login per accedere a questa pagina</h1>
         else if(this.state.error===401)
             return <h1 className="erroreGenericoDiAccesso">Accesso negato</h1>
+
         return (
             <div id="root">
                 {this.state.popUp && <SuccessPopUp message="Inserimento Personale avvenuto con successo!" handleClose={this.closePopUp}/>}
+                {this.state.failurePopUp && <FailurePopUp message="Impossibile inserire il personale, è già presente nel database." handleClose={this.closeFailurePopUp}/>}
                 <Card className="my-10 mx-auto col-xl-7 justify-content-center text-center">
                     <h1>Inserisci credenziali nuovo membro</h1>
                     <form className="mt-4 form-card test" onSubmit={this.handleSubmit}>
